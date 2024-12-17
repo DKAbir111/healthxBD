7cd9be9ae9e65965b42e06be69872826f2dbb6fb
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-12-17 19:16:49 +0600

    sub banner design done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,25 +1,47 @@
 
-import BannerImg from '../../assets/hero_image.png'
+import { FaSearch } from 'react-icons/fa'
+import BannerImg from '../../assets/banner-image.png'
 import './Banner.css'
+import { CiLocationOn } from "react-icons/ci";
 export default function Banner() {
     return (
-        <div >
+        <div className='banner-wrapper'>
             <main className='flex banner-container'>
                 <aside className='left-banner'>
-                    <h4>24/7 EMERGENCY SERVICE</h4>
-                    <h2>Caring of Health Caring of You</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptatibus voluptate obcaecati adipisci optio. Culpa ut velit odio minima perferendis.</p>
-                    <div>
+                    <h4 className='sub-title'>24/7 EMERGENCY SERVICE</h4>
+                    <h2 className='banner-title'>Caring of <span className='highlight'>Health</span> Caring of You</h2>
+                    <p className='section-content'>A brief statement outlining the purpose and mission of the clinic. This can include
+                        the commitment to patient care, community health.</p>
+                    <div className='banner-button'>
                         <button className='btn btn-secondary'>SEE ALL SERVICE</button>
                         <button className='btn btn-primary'>SEE ALL SERVICE</button>
                     </div>
                 </aside>
-                <aside>
+                <aside className='image-container'>
 
-                    <img src={BannerImg} alt="banner image" />
+                    <img src={BannerImg} alt="banner-image" className='banner-image' />
 
                 </aside>
             </main>
+            <div className='sub-banner-container'>
+                <div className='sub-banner'>
+                    <div className="search-container">
+                        <FaSearch className="search-icon" />
+                        <input type="text" className="banner-input" placeholder="Ex. Doctor, Hospital" />
+                    </div>
+                    <div className="search-container">
+                        <FaSearch className="search-icon" />
+                        <input type="text" className="banner-input" placeholder="Ex. Surgeon,Cardiologist" />
+                    </div>
+                    <div className="search-container">
+                        < CiLocationOn className="search-icon" />
+                        <input type="text" className="banner-input" placeholder="Set Your Location" />
+                    </div>
+                    <div className='banner-search'>
+                        <button className='btn btn-primary' style={{ display: 'flex', alignItems: 'center', gap: '5px' }}><FaSearch />  SEARCH</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
